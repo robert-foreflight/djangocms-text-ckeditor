46919a5be4319f81b82e4dc0de22e83616b47eb5
robert-foreflight <robert@foreflight.com>
2023-07-18 10:18:35 -0500

    new bundle

--- djangocms_text_ckeditor/widgets.py
+++ djangocms_text_ckeditor/widgets.py
@@ -17,7 +17,7 @@ from .utils import cms_placeholder_add_plugin
 
 
 # this path is changed automatically whenever you run `gulp bundle`
-PATH_TO_JS = 'djangocms_text_ckeditor/js/dist/bundle-e59c09302f.cms.ckeditor.min.js'
+PATH_TO_JS = 'djangocms_text_ckeditor/js/dist/bundle-47f7494803.cms.ckeditor.min.js'
 
 
 class TextEditorWidget(forms.Textarea):
